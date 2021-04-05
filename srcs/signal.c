0a0ba222c28007e32dc7ce27113c46b47f2052e7
Tripouille <jmichel.gambard@gmail.com>
2021-04-05 08:39:18 -0400

    fix sem in sig_handler

--- srcs/signal.c
+++ srcs/signal.c
@@ -2,21 +2,21 @@
 
 void
 sig_handler(int signal) {
+	bool	is_over = false;
+
 	printf("Catching signal: %i\n", signal);
-	if (g_player.team == DISPLAY) {
-		printf("Map display abort.\n");
-		if (g_player.is_my_turn)
-			sem_op(MAP_SEM, 1, 0);
-	}
-	else {
-		if (!g_player.is_my_turn)
+	if (!g_player.is_my_turn)
 			sem_op(MAP_SEM, -1, 0);
+	if (g_player.team == DISPLAY)
+		printf("Map display abort.\n");
+	else {
 		printf("Team %c player at pos x %i y %i leave the map.\n",
 				g_ipc.shm[pos_to_indice(&g_player.pos)], g_player.pos.x, g_player.pos.y);
 		g_ipc.shm[pos_to_indice(&g_player.pos)] = MAP_EMPTY;
-		sem_op(MAP_SEM, 1, 0);
 	}
-	if (one_team_won()) {
+	is_over = one_team_won();
+	sem_op(MAP_SEM, 1, 0);
+	if (is_over) {
 		sem_destroy();
 		shm_destroy();
 		msg_destroy();
